5a9e7555bbc0fda6997d2fdc33fcab1e751b7844
Marcus Smith <marcusmax94@yahoo.com>
2014-07-31 16:01:43 -0400

    Bringing up the project

---
+++ TheFlyer/gen/edu/msmith/theflyer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.msmith.theflyer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TheFlyer/src/edu/msmith/theflyer/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.msmith.theflyer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements Runnable{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		float num= 10;
+		
+		TextView text= (TextView) findViewById(R.id.text);
+		text.setTranslationX(text.getTranslationX()+num);
+		ViewPropertyAnimator vpa;
+		//vpa.
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
