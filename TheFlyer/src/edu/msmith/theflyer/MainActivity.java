ea5f266e13d3a07706a0ff5cf1dd3d4bdddf0de7
Sherdon <Sherdon@192.168.0.108>
2014-08-16 14:37:39 -0400

    new update

--- TheFlyer/src/edu/msmith/theflyer/MainActivity.java
+++ TheFlyer/src/edu/msmith/theflyer/MainActivity.java
@@ -1,18 +1,24 @@
 package edu.msmith.theflyer;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 // Here is the most basic skeleton for a moving image I could make. 
 // The news Helicopter moves diagonally towards the bottom right side of the screen
-public class MainActivity extends Activity{
 
+public class MainActivity extends Activity{
+	private MediaPlayer ThemeMusic= new MediaPlayer();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// This is going to link our custom canvas with the mainAcitivty one.
 		setContentView(new TestClass1(this));
+		ThemeMusic= MediaPlayer.create(MainActivity.this, R.raw.thememusic);
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		ThemeMusic.start();
 	}
 
 	@Override
@@ -23,9 +29,7 @@ public class MainActivity extends Activity{
 	}
 }
 
-/*
- * private MediaPlayer ThemeMusic= new MediaPlayer();
- * ThemeMusic= MediaPlayer.create(MainActivity.this, R.raw.theme_music_dark_tension);
-		setVolumeControlStream(AudioManager.STREAM_MUSIC);
-		ThemeMusic.start();
-		*/
+
+  
+  
+		
